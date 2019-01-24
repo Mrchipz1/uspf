0ee9000960ba0c9b71d9ed71be29b6a5a040e29c
dhaveed <davidadegoke31@gmail.com>
2019-01-25 00:00:35 +0100

    fixed header issue

--- Transeaze_admin/includes/getData.php
+++ Transeaze_admin/includes/getData.php
@@ -1,4 +1,4 @@
-< ? php
+<?php
 
 if (!Session::exists('id')) {
     Redirect::to('login-log.html');
@@ -7,21 +7,21 @@ if (!Session::exists('id')) {
 $db = new DB;
 $id = session::get('id');
 
-$A = $db - > get('users', array('id', '=', $id));
+$A = $db->get('users', array('id', '=', $id));
 
-$data = $A - > results();
+$data = $A->results();
 
 $sql = 'SELECT * FROM transe_transactions INNER JOIN  users on transe_transactions.user_id = users.id WHERE users.privileged= ?';
 //$TR= $db->action('SELECT *','transe_transaction',array('user_id','=',$id));
-$TR = $db - > query($sql, array($id));
-$Transactions = $TR - > results();
+$TR = $db->query($sql, array($id));
+$Transactions = $TR->results();
 
-$totaltransaction = $TR - > count();
-$TU = $db - > action('SELECT *', 'users', array('privileged', '=', $id));
-$TotalUser = $TU - > count();
+$totaltransaction = $TR->count();
+$TU = $db->action('SELECT *', 'users', array('privileged', '=', $id));
+$TotalUser = $TU->count();
 $sqll = "SELECT SUM(amount) as amount FROM transe_transactions inner join  users on users.id=transe_transactions.user_id WHERE users.PRIVILEGEd = ? AND transe_transactions.status = 'processed' ";
-$TI = $db - > query($sqll, array($id));
-$Totalincome = $TI - > results();
+$TI = $db->query($sqll, array($id));
+$Totalincome = $TI->results();
 
 //$request=$TR->count();
 //print_r($Transactions=$TR->results());
